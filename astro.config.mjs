1ed3d6a2bbb66c5f6ba7c834152fa7a96bd1e4a7
bloom <val.perez@cemep.edu.do>
2023-12-14 12:45:02 -0400

    feat: use static instead of server

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bloom-blog.vercel.app',
   integrations: [mdx(), sitemap(), react()],
-  output: "server",
+  output: "static",
   adapter: vercel()
 });
