e957371e8e62d1ba8e13e8b854dc114b1b84a6ec
bloom <val.perez@cemep.edu.do>
2023-12-14 14:15:00 -0400

    feat: PWA support (no longe using vite-plugin-pwa)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 
 import vercel from "@astrojs/vercel/static";
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,27 +11,4 @@ export default defineConfig({
   integrations: [mdx(), sitemap(), react()],
   output: "static",
   adapter: vercel(),
-  vite: {
-    plugins: [
-      VitePWA({
-        manifest: {
-          name: 'Bloom Blog',
-          short_name: 'Bloom',
-          injectRegister: 'auto',
-          icons: [
-            {
-              src: '/pwa/icons/192.png',
-              sizes: '192x192',
-              type: 'image/png'
-            },
-            {
-              src: '/pwa/icons/512.png',
-              sizes: '512x512',
-              type: 'image/png'
-            }
-          ]
-        }
-      })
-    ]
-  }
 });
