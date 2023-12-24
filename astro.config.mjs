1a7e1bbd8ee0872086aa76daa8eade1bba98c1f0
bloom <val.perez@cemep.edu.do>
2023-12-24 16:19:18 -0400

    chore: remove imagetools, yeah... it made things worser

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
-import { astroImageTools } from "astro-imagetools";
 
 import vercel from "@astrojs/vercel/serverless";
 
@@ -12,35 +11,7 @@ export default defineConfig({
     prefetchAll: true,
   },
   site: 'https://bloom-blog.vercel.app',
-  integrations: [mdx(), sitemap(), react(), astroImageTools({
-    placeholder: "tracedSVG",
-    format: ["webp", "jpg"],
-    fallbackFormat: "png",
-    includeSourceFormat: false,
-    formatOptions: {
-      jpg: {
-        quality: 80,
-      },
-      png: {
-        quality: 80,
-      },
-      webp: {
-        quality: 50,
-      },
-      tracedSVG: {
-        options: {
-          background: "#fff",
-          color: "#000",
-          turnPolicy: "black",
-          turdSize: 1,
-          alphaMax: 1,
-          optCurve: true,
-          threshold: 100,
-          blackOnWhite: false,
-        },
-      },
-    },
-  })],
+  integrations: [mdx(), sitemap(), react()],
   output: "server",
   adapter: vercel(),
 });
